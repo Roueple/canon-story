0d837f1cdd6e0df2eaec48cf023f873cdb0800ba
Roueple <roueple@gmail.com>
2025-06-15 00:16:16 +0700

    Stop tracking script-archive

--- script-archive/chat1-3/install-deps.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-Host "Installing additional dependencies for Chat 2..." -ForegroundColor Green
-
-npm install @clerk/themes svix --save
-
-Write-Host "Dependencies installed successfully!" -ForegroundColor Green
